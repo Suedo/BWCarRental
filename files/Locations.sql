199d2f188a227539e71d37aafe436041d4615ed3
Suedo <somjitnag192@gmail.com>
2019-03-24 11:12:31 +0530

    init

---
+++ files/Locations.sql
@@ -0,0 +1,12 @@
+
+create table Vehicles (
+	id INT,
+	city VARCHAR(9),
+	state VARCHAR(11),
+	country VARCHAR(50)
+);
+insert into Vehicles (id, city, state, country) values (1, 'Delhi', 'Delhi_NCR', 'India');
+insert into Vehicles (id, city, state, country) values (2, 'Mumbai', 'Maharashtra', 'India');
+insert into Vehicles (id, city, state, country) values (3, 'Chennai', 'Tamil_Nadu', 'India');
+insert into Vehicles (id, city, state, country) values (4, 'Bangalore', 'Karnataka', 'India');
+insert into Vehicles (id, city, state, country) values (5, 'Kolkata', 'West_Bengal', 'India');
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminModule } from './admin/admin.module';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AdminModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    EffectsModule.forRoot([AppEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
